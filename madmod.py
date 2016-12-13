2c87a6a9fa8040f954085073b408b36be878492b
seifer08ms <hi181904665@gmail.com>
2016-12-13 17:32:59 +0800

    switch to argparse

--- madmod.py
+++ madmod.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python
-import optparse
+import argparse
 import yaml
 def main():
     """ Runs program and handles command line options """
-    p = optparse.OptionParser(
+    p = argparse.ArgumentParser(
         description= 'module manager for madlib',
         prog='madmod',
-        version='madmod 0.1',
-        usage='%prog [init|add|remove|list]')
-    p.add_option('-i','--init',action='store_true',help='initialize the destination folder of module')
-    p.add_option('-d','--add',action='store_true',help='add the destination folder of module')
-    p.add_option('-l','--list',action='store_true',help='list all modules in configure file')
-    p.add_option('-p','--prefix',action='store_true',help='set the prefix of path for config file')
-    options,arguments = p.parse_args()
-    if len(arguments) < 1:
-        print "Warning:Missing arguments! Please check the usage:"
-        p.print_help()
-    else:
-        if len(arguments) == 1 :
-            print (arguments)
-        if options.prefix:
-            print arguments
-            
+        usage='%(prog)s [options]')
+    p.add_argument('--init',action='store',help='initialize the destination folder of module')
+    p.add_argument('--add',action='store',help='add the destination folder of module')
+    p.add_argument('--list',action='store_true',help='list all modules in configure file')
+    p.add_argument('--prefix',action='store',help='set the prefix of path for config file')
+    p.print_help()
 if __name__ == '__main__':
     main()
