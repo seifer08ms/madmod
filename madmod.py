050cfd653020bf9369ddb7e353d51fe2b2f885de
seifer08ms <hi181904665@gmail.com>
2016-12-13 18:17:24 +0800

    update

--- madmod.py
+++ madmod.py
@@ -11,6 +11,7 @@ def main():
     p.add_argument('--add',action='store',help='add the destination folder of module')
     p.add_argument('--list',action='store_true',help='list all modules in configure file')
     p.add_argument('--prefix',action='store',help='set the prefix of path for config file')
-    p.print_help()
+    p.parse_args()
+#    p.print_help()
 if __name__ == '__main__':
     main()
